407e8ed03d2d657e004a8867e89a7f05c45f02bb
i1yas <ilyasers@gmail.com>
2017-06-28 21:11:32 +0300

    Add App, News, Comments components

--- js/app.js
+++ js/app.js
@@ -1,4 +1,30 @@
+const Comments = function() {
+  return (
+    <div className="comments">
+      No news - no comments.
+    </div>
+  )
+}
+
+const News = function() {
+  return (
+    <div className="news">
+      Unfortunately, there is no news.
+    </div>
+  )
+}
+
+const App = function() {
+  return (
+    <div className="app">
+      Hi, I am App component, here some news:
+      <News />
+      <Comments />
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('root')
 );
